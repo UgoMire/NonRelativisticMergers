bd0a10348ebd064482d5ba274bc12ceb5b5fd85a
UgoMire <ugomire@gmail.com>
2024-04-06 14:15:12 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.48

---
+++ src/NonRelativisticMergers.jl
@@ -0,0 +1,5 @@
+module NonRelativisticMergers
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using NonRelativisticMergers
+using Test
+
+@testset "NonRelativisticMergers.jl" begin
+    # Write your tests here.
+end
