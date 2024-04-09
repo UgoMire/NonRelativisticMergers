19dad8e1bd8ea63e6a27792f18f3fc5f410f85f9
UgoMire <ugomire@gmail.com>
2024-04-09 19:32:59 +0200

    added plot of the reconstruction and flux

--- src/plot.jl
+++ src/plot.jl
@@ -27,3 +27,42 @@ function plot_euler(sol, gd)
 
     return fig
 end
+
+function plot_reconstruction(gd, model, u0)
+    w_reconstruct = reconstruct(model.reconst, gd, u0)
+
+    fig = Figure(size = (1200, 400))
+
+    ax1 = Axis(fig[1, 1])
+    ax2 = Axis(fig[1, 2])
+    ax3 = Axis(fig[1, 3])
+
+    for i = 1:gd.Nx, (ax, j) in zip([ax1, ax2, ax3], 1:3)
+        lines!(
+            ax,
+            [gd.xl[i] - gd.Δx / 2, gd.xl[i] + gd.Δx / 2],
+            [u0[j, i], u0[j, i]],
+            color = :black,
+        )
+        scatter!(ax, [gd.xl[i] - gd.Δx / 2], [w_reconstruct[j, i, 1]], color = :red)
+        scatter!(ax, [gd.xl[i] + gd.Δx / 2], [w_reconstruct[j, i, 2]], color = :blue)
+        lines!(
+            ax,
+            [gd.xl[i] - gd.Δx / 2, gd.xl[i] + gd.Δx / 2],
+            [w_reconstruct[j, i, 1], w_reconstruct[j, i, 2]],
+            color = :grey,
+            linestyle = :dash,
+        )
+    end
+
+    ax4 = Axis(fig[2, 1])
+    ax5 = Axis(fig[2, 2])
+    ax6 = Axis(fig[2, 3])
+
+    for (ax, j) in zip([ax4, ax5, ax6], 1:3)
+        F = solve_riemann(model.riemannsolver, gd, w_reconstruct)
+        lines!(ax, gd.xl, F[j, :])
+    end
+
+    return fig
+end
