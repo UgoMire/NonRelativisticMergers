cda20cb18b3d9bad7b263ac440056fa0b43e7976
UgoMire <ugomire@gmail.com>
2024-04-13 21:41:44 +0200

    uniform 2d grid implementation

--- src/types.jl
+++ src/types.jl
@@ -12,6 +12,29 @@ struct Grid1D <: Grid
         new(xmin, xmax, Nx, Δx, xl)
     end
 end
+struct Grid2D <: Grid
+    xmin::Float64
+    xmax::Float64
+    Nx::Int64
+    Δx::Float64
+    xl::Vector{Float64}
+
+    ymin::Float64
+    ymax::Float64
+    Ny::Int64
+    Δy::Float64
+    yl::Vector{Float64}
+
+    function Grid2D(; xmin = 0, xmax = 1, ymin = 0, ymax = 1, Nx = 100, Ny = 100)
+        xl = range(xmin, xmax, Nx)
+        Δx = xl[2] - xl[1]
+
+        yl = range(ymin, ymax, Ny)
+        Δy = yl[2] - yl[1]
+
+        new(xmin, xmax, Nx, Δx, xl, ymin, ymax, Ny, Δy, yl)
+    end
+end
 
 abstract type FDModel end
 struct Euler <: FDModel
