8a879cc3f4e28e3cb18d4ec83dcbed222ec0a231
UgoMire <ugomire@gmail.com>
2024-04-14 22:26:46 +0200

    Refactor variable names and use destructuring in 2d-euler.jl

--- src/reconstructor.jl
+++ src/reconstructor.jl
@@ -29,9 +29,9 @@ function reconstruct!(prob::FDProblem{Grid2D,<:Any,Constant,<:Any}, wstore, u)
         iyp = iy == Ny ? 1 : iy + 1
 
         # Get the primitive variables.
-        ρ, vx, vy, p = get_primitive_variables(prob, u, ix, iy)
-        ρ_px, vx_px, vy_px, p_px = get_primitive_variables(prob, u, ixp, iy)
-        ρ_py, vx_py, vy_py, p_py = get_primitive_variables(prob, u, ix, iyp)
+        ρ, vx, vy, P = get_primitive_variables(prob, u, ix, iy)
+        ρ_px, vx_px, vy_px, P_px = get_primitive_variables(prob, u, ixp, iy)
+        ρ_py, vx_py, vy_py, P_py = get_primitive_variables(prob, u, ix, iyp)
 
         # Reconstruct the density.
         wstore[1, ix, iy, 1] = ρ
@@ -39,22 +39,23 @@ function reconstruct!(prob::FDProblem{Grid2D,<:Any,Constant,<:Any}, wstore, u)
         wstore[1, ix, iy, 3] = ρ
         wstore[1, ix, iy, 4] = ρ_py
 
-        # Reconstruct the velocity.
+        # Reconstruct the x velocity.
         wstore[2, ix, iy, 1] = vx
         wstore[2, ix, iy, 2] = vx_px
         wstore[2, ix, iy, 3] = vx
         wstore[2, ix, iy, 4] = vx_py
 
+        # Reconstruct the y velocity.
         wstore[3, ix, iy, 1] = vy
         wstore[3, ix, iy, 2] = vy_px
         wstore[3, ix, iy, 3] = vy
         wstore[3, ix, iy, 4] = vy_py
 
         # Reconstruct the pressure.
-        wstore[4, ix, iy, 1] = p
-        wstore[4, ix, iy, 2] = p_px
-        wstore[4, ix, iy, 3] = p
-        wstore[4, ix, iy, 4] = p_py
+        wstore[4, ix, iy, 1] = P
+        wstore[4, ix, iy, 2] = P_px
+        wstore[4, ix, iy, 3] = P
+        wstore[4, ix, iy, 4] = P_py
     end
 end
 
