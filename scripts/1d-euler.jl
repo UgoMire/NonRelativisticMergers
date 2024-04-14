0131debd49fc96796d66b9a8d8bbe36c5915d2f7
UgoMire <ugomire@gmail.com>
2024-04-14 22:40:02 +0200

    updated scripts

--- scripts/1d-euler.jl
+++ scripts/1d-euler.jl
@@ -7,7 +7,7 @@ global_logger(TerminalLogger())
 gd = Grid1D(; xmin = -1, xmax = 2, Nx = 300)
 
 # ρ0l = ones(gd.Nx)
-ρ0l = 0.1 .+ map(x -> 0.1 * exp(-100 * (x - 0.5)^2), gd.xl)
+ρ0l = 1 .+ map(x -> 1 * exp(-100 * (x - 0.5)^2), gd.xl)
 # ρ0l = [0.4 < x < 0.6 ? 0.5 : 0.05 for x in gd.xl]
 
 v0l = zeros(gd.Nx)
@@ -17,17 +17,17 @@ v0l = zeros(gd.Nx)
 # p0l = ones(gd.Nx)
 # p0l = zeros(gd.Nx)
 # p0l = 0.1 .+ map(x -> 1.8 * exp(-100 * (x - 0.5)^2), gd.xl)
-p0l = 0.1 .+ map(x -> 0.4 * exp(-100 * (x - 0.5)^2), gd.xl)
+p0l = 1 .+ map(x -> 0.1 * exp(-100 * (x - 0.5)^2), gd.xl)
 # p0l = [0.4 < x < 0.6 ? 0.5 : 0.1 for x in gd.xl]
 
-tspan = (0, 0.046)
+tspan = (0, 4.146)
 
 # reconstructor = Constant()
 # reconstructor = MUSCL()
 reconstructor = KT()
 
-# riemannsolver = NaiveRS()
-riemannsolver = HLLC()
+riemannsolver = NaiveRS()
+# riemannsolver = HLLC()
 
 # model = Euler(Constant(), NaiveRS(), ρ0l, v0l, p0l)
 # model = Euler(; reconst, riemannsolver)
@@ -48,9 +48,8 @@ u0 = sol.u[end]
 plot_reconstruction(gd, model, u0)
 
 ##
-reconst = Constant()
-# reconst = MUSCL()
-# reconstructor = KT()
+# reconst = Constant()
+reconstructor = KT()
 
 # riemannsolver = NaiveRS()
 riemannsolver = HLLC()
--- scripts/2d-euler.jl
+++ scripts/2d-euler.jl
@@ -3,12 +3,12 @@ using GLMakie
 
 gd = Grid2D(; xmin = -1, xmax = 1, ymin = -1, ymax = 1, Nx = 100, Ny = 100)
 
-ρ0l = [1.0 + exp(-100 * (x^2 + y^2)) for x in gd.xl, y in gd.yl]
-vx0l = [0.0 for x in gd.xl, y in gd.yl]
-vy0l = [0.0 for x in gd.xl, y in gd.yl]
-p0l = [1.0 + exp(-100 * (x^2 + y^2)) for x in gd.xl, y in gd.yl]
+ρ0 = [1.0 + exp(-100 * (x^2 + y^2)) for x in gd.xl, y in gd.yl]
+vx0 = [0.0 for x in gd.xl, y in gd.yl]
+vy0 = [0.0 for x in gd.xl, y in gd.yl]
+P0 = [1.0 + 0.01 * exp(-100 * (x^2 + y^2)) for x in gd.xl, y in gd.yl]
 
-tspan = (0, 5)
+tspan = (0, 0.15)
 
 reconstructor = Constant()
 # reconstructor = KT()
@@ -18,14 +18,14 @@ riemannsolver = NaiveRS()
 
 prob = FDProblem(gd, Euler(), reconstructor, riemannsolver)
 
-sol = solve(prob, ρ0l, vx0l, vy0l, p0l, tspan);
+sol = solve(prob, ρ0, vx0, vy0, P0, tspan);
 
 plot_euler2d(prob, sol)
 
 ## Benchmarking.
 using PreallocationTools
 
-u0 = NonRelativisticMergers.setup_initial_state(prob, ρ0l, vx0l, vy0l, p0l)
+u0 = NonRelativisticMergers.setup_initial_state(prob, ρ0, vx0, vy0, P0)
 
 wstore = zeros(4, prob.grid.Nx, prob.grid.Ny, 4)
 xfluxstore = zeros(4, prob.grid.Nx, prob.grid.Ny)
