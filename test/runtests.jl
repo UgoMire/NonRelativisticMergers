5c2bc04de3bcd78fce656691826df8b2b2e10134
UgoMire <ugomire@gmail.com>
2024-04-14 22:24:34 +0200

    added test for 2d hydrostatic

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,9 @@
 using NonRelativisticMergers
 using Test
 
-@testset verbose = true "1d Euler - Hydrostatic flow is time independent" begin
+verbose = true
+
+@testset verbose = verbose "1d Euler - Hydrostatic flow is time independent" begin
     grid = Grid1D(; xmin = 0, xmax = 1, Nx = 100)
 
     ρ0l = ones(grid.Nx)
@@ -23,5 +25,34 @@ using Test
         @test ρ0l ≈ ρend
         @test v0l ≈ vend
         @test p0l ≈ pend
+    end
+end
+
+@testset verbose = verbose "2d Euler - Hydrostatic flow is time independent" begin
+    grid = Grid2D(; xmin = 0, xmax = 1, ymin = 0, ymax = 1, Nx = 100, Ny = 100)
+
+    ρ = ones(grid.Nx, grid.Ny)
+    vx = zeros(grid.Nx, grid.Ny)
+    vy = zeros(grid.Nx, grid.Ny)
+    p = ones(grid.Nx, grid.Ny)
+
+    tspan = (0, 1)
+
+    @testset "- $reconstructor, $riemannsolver" for reconstructor in (Constant(),)
+        riemannsolver in (NaiveRS(),)
+
+        prob = FDProblem(grid, Euler(), reconstructor, riemannsolver)
+        sol = solve(prob, ρ, vx, vy, p, tspan)
+
+        ρend = sol.u[end][1, :, :]
+        vxend = sol.u[end][2, :, :] ./ ρend
+        vyend = sol.u[end][3, :, :] ./ ρend
+        pend =
+            @. (sol.u[end][4, :, :] - 0.5 * ρend * (vxend^2 + vyend^2)) * (prob.model.γ - 1)
+
+        @test ρ ≈ ρend
+        @test vx ≈ vxend
+        @test vy ≈ vyend
+        @test p ≈ pend
     end
 end
