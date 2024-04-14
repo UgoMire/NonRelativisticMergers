c50341da85d9c0c23c1b9e0c10b4faad682145ca
UgoMire <ugomire@gmail.com>
2024-04-14 11:33:47 +0200

    updated scripts

--- scripts/1d-euler.jl
+++ scripts/1d-euler.jl
@@ -7,7 +7,7 @@ global_logger(TerminalLogger())
 gd = Grid1D(; xmin = -1, xmax = 2, Nx = 300)
 
 # ρ0l = ones(gd.Nx)
-ρ0l = 0.1 .+ map(x -> 0.1 * exp(-100 * (x - 0.5)^2), gd.xl)
+ρ0l = 1.0 .+ map(x -> 0.1 * exp(-100 * (x - 0.5)^2), gd.xl)
 # ρ0l = [0.4 < x < 0.6 ? 0.5 : 0.05 for x in gd.xl]
 
 v0l = zeros(gd.Nx)
@@ -17,7 +17,7 @@ v0l = zeros(gd.Nx)
 # p0l = ones(gd.Nx)
 # p0l = zeros(gd.Nx)
 # p0l = 0.1 .+ map(x -> 1.8 * exp(-100 * (x - 0.5)^2), gd.xl)
-p0l = 0.1 .+ map(x -> 0.4 * exp(-100 * (x - 0.5)^2), gd.xl)
+p0l = 1.0 .+ map(x -> 0.4 * exp(-100 * (x - 0.5)^2), gd.xl)
 # p0l = [0.4 < x < 0.6 ? 0.5 : 0.1 for x in gd.xl]
 
 tspan = (0, 5.053)
--- scripts/2d-euler.jl
+++ scripts/2d-euler.jl
@@ -1,4 +1,33 @@
 using NonRelativisticMergers
 using GLMakie
 
-gd = Grid2D(; xmin = -1, xmax = 2, ymin = -1, ymax = 2, Nx = 100, Ny = 100)
+gd = Grid2D(; xmin = -1, xmax = 1, ymin = -1, ymax = 1, Nx = 100, Ny = 100)
+
+ρ0l = [1.0 + exp(-100 * (x^2 + y^2)) for x in gd.xl, y in gd.yl]
+v0l = [0.0 for x in gd.xl, y in gd.yl]
+p0l = [0.0 for x in gd.xl, y in gd.yl]
+
+tspan = (0, 5)
+
+reconstructor = Constant()
+reconstructor = KT()
+
+riemannsolver = NaiveRS()
+riemannsolver = HLLC()
+
+prob = FDProblem(gd, Euler(), reconstructor, riemannsolver)
+
+# sol = solve(prob, ρ0l, v0l, p0l, tspan)
+
+## Benchmarking.
+using PreallocationTools
+
+u0 = NonRelativisticMergers.get_initial_conditions(prob, ρ0l, v0l, p0l)
+
+wstore = DiffCache(zeros(3, prob.gd.Nx, prob.gd.Ny, 2))
+fluxstore = DiffCache(zeros(3, prob.gd.Nx, prob.gd.Ny))
+
+du = copy(u0)
+p = (; prob, wstore, fluxstore)
+
+@btime NonRelativisticMergers.euler2d!(du, u0, p, tspan[1])
