1d19b60afdc4b6995502bc4f92afca79b003efce
UgoMire <ugomire@gmail.com>
2024-04-16 23:28:54 +0200

    updated script

--- scripts/2d-euler.jl
+++ scripts/2d-euler.jl
@@ -6,17 +6,17 @@ global_logger(TerminalLogger())
 
 gd = Grid2D(; xmin = -1, xmax = 1, ymin = -1, ymax = 1, Nx = 100, Ny = 100)
 
-# ρ0 = [1.0 + exp(-100 * (x^2 + y^2)) for x in gd.xl, y in gd.yl]
-ρ0 = [-0.2 < x < 0.2 && -0.2 < y < 0.2 ? 2.0 : 1.0 for x in gd.xl, y in gd.yl]
+ρ0 = [10.0 + exp(-100 * (x^2 + y^2)) for x in gd.xl, y in gd.yl]
+# ρ0 = [-0.2 < x < 0.2 && -0.2 < y < 0.2 ? 2.0 : 1.0 for x in gd.xl, y in gd.yl]
 
 vx0 = [0.0 for x in gd.xl, y in gd.yl]
 
 vy0 = [0.0 for x in gd.xl, y in gd.yl]
 
-# P0 = [1.0 + 2 * exp(-100 * (x^2 + y^2)) for x in gd.xl, y in gd.yl]
-P0 = [-0.2 < x < 0.2 && -0.2 < y < 0.2 ? 2.0 : 1.0 for x in gd.xl, y in gd.yl]
+P0 = [10.0 + 1 * exp(-100 * (x^2 + y^2)) for x in gd.xl, y in gd.yl]
+# P0 = [-0.2 < x < 0.2 && -0.2 < y < 0.2 ? 2.0 : 1.0 for x in gd.xl, y in gd.yl]
 
-tspan = (0, 1.15)
+tspan = (0, 0.045)
 
 # reconstructor = Constant()
 reconstructor = KT()
@@ -31,7 +31,13 @@ sol = solve(prob, ρ0, vx0, vy0, P0, tspan);
 plot_euler2d(prob, sol)
 
 ## Benchmarking.
-using PreallocationTools
+# reconstructor = Constant()
+reconstructor = KT()
+
+# riemannsolver = NaiveRS()
+riemannsolver = HLLC()
+
+prob = FDProblem(gd, Euler(), reconstructor, riemannsolver)
 
 u0 = NonRelativisticMergers.setup_initial_state(prob, ρ0, vx0, vy0, P0)
 
