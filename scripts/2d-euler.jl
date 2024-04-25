02f16645cf6bbd4db8968cd2a2670aa32e117490
UgoMire <ugomire@gmail.com>
2024-04-25 12:43:14 +0200

    updated script

--- scripts/2d-euler.jl
+++ scripts/2d-euler.jl
@@ -4,8 +4,8 @@ using Logging: global_logger
 using TerminalLoggers: TerminalLogger
 global_logger(TerminalLogger())
 
-gd = Grid2D(; xmin = -2, xmax = 2, ymin = -2, ymax = 2, Nx = 100, Ny = 100)
-gd = Grid2D(; xmin = -2, xmax = 2, ymin = -2, ymax = 2, Nx = 100, Ny = 100)
+gd = Grid2D(; xmin = -2, xmax = 2, ymin = -2, ymax = 2, Nx = 200, Ny = 200)
+gd = Grid2D(; xmin = -2, xmax = 2, ymin = -2, ymax = 2, Nx = 200, Ny = 200)
 
 ρ0 = [1.0 for x in gd.xl, y in gd.yl]
 # ρ0 = [1.0 + exp(-100 * (x^2 + y^2)) for x in gd.xl, y in gd.yl]
@@ -16,8 +16,8 @@ gd = Grid2D(; xmin = -2, xmax = 2, ymin = -2, ymax = 2, Nx = 100, Ny = 100)
 # ρ0 = [-0.2 < x < 0.2 && -0.2 < y < 0.2 ? 2.0 : 1.0 for x in gd.xl, y in gd.yl]
 # ρ0 = [1.0 + exp(-100 * (x^2 + y^2 - 1)^2) for x in gd.xl, y in gd.yl]
 
-vx0 = [0.0 for x in gd.xl, y in gd.yl]
-# vx0 = [1.0 for x in gd.xl, y in gd.yl]
+# vx0 = [0.0 for x in gd.xl, y in gd.yl]
+vx0 = [1.0 for x in gd.xl, y in gd.yl]
 # vx0 = [1.0 - 10y * exp(-10 * (x^2 + y^2)) for x in gd.xl, y in gd.yl]
 # vx0 = [-y / (2π * (x^2 + y^2)) for x in gd.xl, y in gd.yl]
 
@@ -30,7 +30,7 @@ P0 = [1.0 + 10 * exp(-10 * (x^2 + y^2)) for x in gd.xl, y in gd.yl]
 # P0 = [1.0 + exp(-100 * x^2) for x in gd.xl, y in gd.yl]
 # P0 = [1.0 + exp(-100 * (x^2 + y^2 - 1)^2) for x in gd.xl, y in gd.yl]
 
-tspan = (0, 0.550)
+tspan = (0, 1.150)
 
 # reconstructor = Constant()
 reconstructor = KT()
@@ -65,3 +65,24 @@ du = copy(u0)
 p = (; prob, wstore, xfluxstore, yfluxstore)
 
 @btime NonRelativisticMergers.euler2d!(du, u0, p, tspan[1])
+##
+
+@code_warntype NonRelativisticMergers.euler2d!(du, u0, p, tspan[1])
+@code_warntype NonRelativisticMergers.hllc_riemann_solver(
+    prob,
+    (; ρ = 1, vx = 1, vy = 1, P = 1),
+    (; ρ = 1, vx = 1, vy = 1, P = 1),
+    (; x = 1, y = 0),
+)
+@code_warntype NonRelativisticMergers.hllc_riemann_solver(
+    prob,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    (; x = 1, y = 0),
+)
