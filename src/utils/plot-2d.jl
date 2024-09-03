50f155e9d6577e8d464ea5af7581ea277d6ac076
UgoMire <ugomire@gmail.com>
2024-09-03 22:10:49 +0200

    updated plot script

--- src/utils/plot-2d.jl
+++ src/utils/plot-2d.jl
@@ -1,148 +1,74 @@
-# function plot_euler(
-#         prob::FDProblem{Grid2D, Euler, <:Any, <:Any},
-#         sol;
-#         type = :heatmap
-# )
-#     (; xl, yl) = prob.grid
+function plot_euler(
+        prob::FDProblem{Grid2D, Euler, <:Any, <:Any},
+        sol;
+        type = :heatmap
+)
+    (; xl, yl) = prob.grid
 
-#     tspan = (sol.t[1], sol.t[end])
+    tspan = (sol.t[1], sol.t[end])
 
-#     fig = Figure(; size = (900, 900))
+    fig = Figure(; size = (900, 900))
 
-#     if type == :heatmap
-#         sg = SliderGrid(fig[2, 1:2], (label = "t", range = range(tspan[1], tspan[2], 100)))
-#         tlift = sg.sliders[1].value
+    if type == :heatmap
+        sg = SliderGrid(fig[2, 1:2], (label = "t", range = range(tspan[1], tspan[2], 100)))
+        tlift = sg.sliders[1].value
 
-#         Na = 5 # Plot an arrow one every Na point. 
-#         ax1 = Axis(fig[1, 1]; title = "density", xlabel = "x", ylabel = "y")
-#         heatmap!(ax1, xl, yl, (@lift sol($tlift)[1, :, :]))
-#         arrows!(
-#             ax1,
-#             xl[1:Na:end],
-#             yl[1:Na:end],
-#             (@lift sol($tlift)[2, 1:Na:end, 1:Na:end] ./
-#                    sol($tlift)[1, 1:Na:end, 1:Na:end]),
-#             (@lift sol($tlift)[3, 1:Na:end, 1:Na:end] ./
-#                    sol($tlift)[1, 1:Na:end, 1:Na:end]);
-#             arrowsize = 10,
-#             lengthscale = 0.5
-#         )
+        Na = 5 # Plot an arrow one every Na point. 
+        ax1 = Axis(fig[1, 1]; title = "density", xlabel = "x", ylabel = "y")
+        heatmap!(ax1, xl, yl, (@lift sol($tlift)[1, :, :]); colormap = :lipari)
+        arrows!(
+            ax1,
+            xl[1:Na:end],
+            yl[1:Na:end],
+            (@lift sol($tlift)[2, 1:Na:end, 1:Na:end] ./
+                   sol($tlift)[1, 1:Na:end, 1:Na:end]),
+            (@lift sol($tlift)[3, 1:Na:end, 1:Na:end] ./
+                   sol($tlift)[1, 1:Na:end, 1:Na:end]);
+            arrowsize = 10,
+            lengthscale = 0.5,
+            normalize = true
+        )
 
-#         ax4 = Axis(fig[1, 2]; title = "pressure", xlabel = "x")
-#         heatmap!(
-#             ax4,
-#             xl,
-#             yl,
-#             (@lift (5 / 3 - 1) * (
-#                 sol($tlift)[4, :, :] .-
-#                 1 / 2 * sol($tlift)[1, :, :] .*
-#                 (sol($tlift)[2, :, :] .^ 2 + sol($tlift)[3, :, :] .^ 2)
-#             ));
-#         )
-#     elseif type == :surface
-#         sg = SliderGrid(fig[3, 1:2], (label = "t", range = range(tspan[1], tspan[2], 100)))
-#         tlift = sg.sliders[1].value
+        ax4 = Axis(fig[1, 2]; title = "pressure", xlabel = "x")
+        heatmap!(
+            ax4,
+            xl,
+            yl,
+            (@lift (5 / 3 - 1) * (
+                sol($tlift)[4, :, :] .-
+                1 / 2 * sol($tlift)[1, :, :] .*
+                (sol($tlift)[2, :, :] .^ 2 + sol($tlift)[3, :, :] .^ 2)
+            ));
+            colormap = :lipari
+        )
+    elseif type == :surface
+        sg = SliderGrid(fig[3, 1:2], (label = "t", range = range(tspan[1], tspan[2], 100)))
+        tlift = sg.sliders[1].value
 
-#         ax1 = Axis3(fig[1, 1]; title = "density", xlabel = "x", ylabel = "y")
-#         surface!(ax1, xl, yl, (@lift sol($tlift)[1, :, :]))
+        ax1 = Axis3(fig[1, 1]; title = "density", xlabel = "x", ylabel = "y")
+        surface!(ax1, xl, yl, (@lift sol($tlift)[1, :, :]))
 
-#         ax3 = Axis3(fig[2, 1]; title = "x velocity", xlabel = "x")
-#         surface!(ax3, xl, yl, (@lift sol($tlift)[2, :, :] ./ sol($tlift)[1, :, :]))
+        ax3 = Axis3(fig[2, 1]; title = "x velocity", xlabel = "x")
+        surface!(ax3, xl, yl, (@lift sol($tlift)[2, :, :] ./ sol($tlift)[1, :, :]))
 
-#         ax3 = Axis3(fig[2, 2]; title = "y velocity", xlabel = "x")
-#         surface!(ax3, xl, yl, (@lift sol($tlift)[3, :, :] ./ sol($tlift)[1, :, :]))
+        ax3 = Axis3(fig[2, 2]; title = "y velocity", xlabel = "x")
+        surface!(ax3, xl, yl, (@lift sol($tlift)[3, :, :] ./ sol($tlift)[1, :, :]))
 
-#         ax4 = Axis3(fig[1, 2]; title = "pressure", xlabel = "x")
-#         surface!(
-#             ax4,
-#             xl,
-#             yl,
-#             (@lift (5 / 3 - 1) * (
-#                 sol($tlift)[4, :, :] .-
-#                 1 / 2 * sol($tlift)[1, :, :] .*
-#                 (sol($tlift)[2, :, :] .^ 2 + sol($tlift)[3, :, :] .^ 2)
-#             ));
-#         )
-#     end
+        ax4 = Axis3(fig[1, 2]; title = "pressure", xlabel = "x")
+        surface!(
+            ax4,
+            xl,
+            yl,
+            (@lift (5 / 3 - 1) * (
+                sol($tlift)[4, :, :] .-
+                1 / 2 * sol($tlift)[1, :, :] .*
+                (sol($tlift)[2, :, :] .^ 2 + sol($tlift)[3, :, :] .^ 2)
+            ));
+        )
+    end
 
-#     return fig
-# end
-
-# function plot_euler(
-#         prob::FDProblem{Grid2D, EulerSelfGravity, <:Any, <:Any},
-#         sol;
-#         type = :heatmap
-# )
-#     (; xl, yl) = prob.grid
-
-#     tspan = (sol.t[1], sol.t[end])
-
-#     fig = Figure(; size = (900, 900))
-
-#     if type == :heatmap
-#         sg = SliderGrid(fig[2, 1:2], (label = "t", range = range(tspan[1], tspan[2], 100)))
-#         tlift = sg.sliders[1].value
-
-#         Na = 10 # Plot an arrow one every Na point. 
-#         ax1 = Axis(fig[1, 1]; title = "density", xlabel = "x", ylabel = "y")
-#         contourf!(
-#             ax1, xl, yl, (@lift sol($tlift)[1, :, :]); colormap = :inferno, levels = 20)
-#         arrows!(
-#             ax1,
-#             xl[1:Na:end],
-#             yl[1:Na:end],
-#             (@lift sol($tlift)[2, 1:Na:end, 1:Na:end] ./
-#                    sol($tlift)[1, 1:Na:end, 1:Na:end]),
-#             (@lift sol($tlift)[3, 1:Na:end, 1:Na:end] ./
-#                    sol($tlift)[1, 1:Na:end, 1:Na:end]);
-#             arrowsize = 10,
-#             lengthscale = 0.2,
-#             color = (:grey, 0.5)
-#         )
-
-#         ax4 = Axis(fig[1, 2]; title = "pressure", xlabel = "x")
-#         heatmap!(
-#             ax4,
-#             xl,
-#             yl,
-#             (@lift (5 / 3 - 1) * (
-#                 sol($tlift)[4, :, :] .-
-#                 1 / 2 * sol($tlift)[1, :, :] .*
-#                 (sol($tlift)[2, :, :] .^ 2 + sol($tlift)[3, :, :] .^ 2)
-#             ));
-#             colormap = :inferno
-#         )
-#     elseif type == :surface
-#         sg = SliderGrid(fig[3, 1:2], (label = "t", range = range(tspan[1], tspan[2], 100)))
-#         tlift = sg.sliders[1].value
-
-#         ax1 = Axis3(fig[1, 1]; title = "density", xlabel = "x", ylabel = "y")
-#         surface!(ax1, xl, yl, (@lift sol($tlift)[1, :, :]))
-
-#         ax3 = Axis3(fig[2, 1]; title = "x velocity", xlabel = "x")
-#         surface!(ax3, xl, yl, (@lift sol($tlift)[2, :, :] ./ sol($tlift)[1, :, :]))
-
-#         ax3 = Axis3(fig[2, 2]; title = "y velocity", xlabel = "x")
-#         surface!(ax3, xl, yl, (@lift sol($tlift)[3, :, :] ./ sol($tlift)[1, :, :]))
-
-#         ax4 = Axis3(fig[1, 2]; title = "pressure", xlabel = "x")
-#         surface!(
-#             ax4,
-#             xl,
-#             yl,
-#             (@lift (5 / 3 - 1) * (
-#                 sol($tlift)[4, :, :] .-
-#                 1 / 2 * sol($tlift)[1, :, :] .*
-#                 (sol($tlift)[2, :, :] .^ 2 + sol($tlift)[3, :, :] .^ 2)
-#             ));
-#         )
-
-#         ax5 = Axis3(fig[1, 3]; title = "potential", xlabel = "x")
-#         surface!(ax5, xl, yl, (@lift solve_poisson(prob, sol($tlift)[1, :, :])))
-#     end
-
-#     return fig
-# end
+    return fig
+end
 
 function add_main_layout!(fig, prob, sol; cmap = :lipari, Na = 10)
     (; xmin, xmax, Nx, xl, ymin, ymax, Ny, yl) = prob.grid
@@ -163,9 +89,9 @@ function add_main_layout!(fig, prob, sol; cmap = :lipari, Na = 10)
         ax = Axis(fig[i, j][1, 1]; title, xlabel = "x", ylabel = "y")
         limits!(ax, (xmin, xmax), (ymin, ymax))
 
-        hm = heatmap!(ax, xl, yl, quantity; colormap = cmap, interpolate = true)
+        hm = heatmap!(ax, xl, yl, quantity; colormap = cmap, interpolate = false)
         arrows!(ax, xl[1:Na:end], yl[1:Na:end], vxlift, vylift;
-            arrowsize = 6, lengthscale = 0.08, color = (:grey, 0.1), normalize = true)
+            arrowsize = 6, lengthscale = 0.08, color = (:grey, 0.4), normalize = true)
 
         Colorbar(fig[i, j][1, 2], hm; tickformat)
     end
